a8015c011e05083d2e801f1d296a116d4c2fcced
Ahmet Kilinc <akx9@icloud.com>
2022-07-31 22:30:13 +0100

    change title

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,7 +8,7 @@ const Profile = () => {
 				src="https://avatars.githubusercontent.com/u/37756565"
 				alt="Profile of Ahmet"
 			/>
-			<h1 className="profile-name">Ahmet Kılınç</h1>
+			<h1 className="profile-name">Find me below</h1>
 		</div>
 	);
 };
