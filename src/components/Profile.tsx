fcc172f9822b6be6a1cea4aadf38e31221ac8206
Ahmet Kilinc <akx9@icloud.com>
2022-10-07 17:25:46 +0100

    update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,11 +3,7 @@ import "../assets/styles/Profile.css";
 const Profile = () => {
 	return (
 		<div className="profile">
-			<img
-				className="profile-photo"
-				src="https://avatars.githubusercontent.com/u/37756565"
-				alt="Profile of Ahmet"
-			/>
+			<img className="profile-photo" src="/image.jpeg" alt="Profile of Ahmet" />
 			<h1 className="profile-name">Find me below</h1>
 		</div>
 	);
